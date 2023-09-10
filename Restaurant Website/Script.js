c9be4f597e5203b556f48637a455f95e95e38dfd
Prashant Atmaram Pawar <104372381+itpap@users.noreply.github.com>
2023-09-10 22:02:57 +0530

    Add files via upload

---
+++ Restaurant Website/Script.js
@@ -0,0 +1,7 @@
+// JavaScript for responsive navigation
+const navToggle = document.querySelector('.nav-toggle');
+const navMenu = document.querySelector('nav ul');
+
+navToggle.addEventListener('click', () => {
+    navMenu.classList.toggle('show');
+});
